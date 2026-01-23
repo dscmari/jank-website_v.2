62c406616efc5ef233648b7589b3aceebfe9a1f4
Marian Noekel <mariannoekel@gmail.com>
2026-01-23 20:31:04 +0100

    add basic text to /danke

--- app/(main)/danke/page.tsx
+++ app/(main)/danke/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>danke</div>
+    <div className='p-4 md:p-16 lg:p-32 text-center'>
+      <h1>Vielen Dank für deine Kontaktaufnahme!</h1>
+      <h2>Dein Formular wurde erfolgreich versendet. Ich melde mich möglichst bald bei dir.</h2>
+    </div>
   )
 }
