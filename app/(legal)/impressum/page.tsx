1406ca05cf5c0af4efd7c0629797549244007d32
dscmari <mariannoekel@gmail.com>
2026-02-03 11:28:40 +0100

    adjust favicon

--- app/(legal)/impressum/page.tsx
+++ app/(legal)/impressum/page.tsx
@@ -12,7 +12,7 @@ export default function page() {
         <span>Jan Krösche - SEO & Google Ads Beratung</span>
         <span>Obere Mühlstraße 6</span>
         <span>86825 Bad Wörishofen</span>
-        <span>Umsatzsteuer ID: 138 / 241 / 20581</span>
+        <span>Steuernummer: 138 / 241 / 20581</span>
       </div>
       <div className="flex flex-col">
         <h2 className="mt-4">Kontakt</h2>
