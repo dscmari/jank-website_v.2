541b371189d60333311c85c45e8eacea9b6e7ba8
dscmari <mariannoekel@gmail.com>
2026-04-20 15:01:53 +0200

    adjust workshop box

--- app/(main)/seo-geo-ads-workshops/page.tsx
+++ app/(main)/seo-geo-ads-workshops/page.tsx
@@ -1,5 +1,6 @@
 import Breadcrumbs from "@/app/components/Breadcrumps";
 import Pricing from "@/app/components/pricing/Pricing";
+import WorkshopPricingBoxes from "@/app/components/WorkshopPricingBoxes";
 import { workshops } from "@/app/content/pricingBoxesContent";
 import { Metadata } from "next";
 
@@ -14,7 +15,7 @@ export default function page() {
       <h1 className="text-center lg:text-start px-4 lg:px-32 pt-24 pb-4 lg:pb-8">
         Unsere Workshops
       </h1>
-      <Pricing
+      <WorkshopPricingBoxes
         pricingBoxes={workshops}
         className="px-4 lg:px-32 pb-24 lg:pb-32"
       />
---
+++ app/components/WorkshopPricingBoxes.tsx
@@ -0,0 +1,68 @@
+import { IoIosCheckmarkCircle } from "react-icons/io";
+
+type WorkshopPricingBox = {
+  icon: string;
+  title: string;
+  subTitle: string;
+  introText: string;
+  services: string[];
+  closingText: string;
+  outroText?: React.ReactNode;
+  contactText?: string;
+}
+
+type Props = {
+  className?: string;
+  pricingBoxes: WorkshopPricingBox[]
+};
+
+export default function WorkshopPricingBoxes({ className, pricingBoxes }: Props) {
+  return (
+    <section className={`${className}`}>
+      <div className="grid grids-cols-1 xl:grid-cols-2 gap-12">
+        {pricingBoxes.map((e, index) => (
+          <div
+            key={index}
+            className="bg-white flex flex-col justify-between xl:m-0 p-8 rounded-lg dark"
+          >
+            <div>
+              <div className="flex flex-col items-start gap-2">
+                <span className="text-white bg-custom-red font-semibold tracking-tight py-1 px-2 rounded-lg">
+                  {e.icon}
+                </span>
+                <span className="font-semibold tracking-tight">{e.title}</span>
+                <span className="font-light">{e.subTitle}</span>
+              </div>
+
+              <h1 className="text-center mt-16">{e.title}</h1>
+              <p className="mt-4 mb-8 font-light text-sm">{e.introText}</p>
+              <span className="font-semibold tracking-tight">Du lernst</span>
+              <div key={index} className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 gap-2 mt-2">
+                {e.services.map((service, index) => (
+                  <div key={index} className="flex items-start gap-2">
+                    <IoIosCheckmarkCircle color="rgb(231,0,53)" size={24} className="shrink-0" />
+                    <span>{service}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="mt-8">
+              <p className="pb-8 text-sm font-light border-b-1 border-black/20">
+                {e.closingText}
+              </p>
+              <div>
+                <span className="mt-4 block text-center font-light">
+                    {e.contactText ? e.contactText : "Preisanfrage unter"}
+                </span>
+                <span className="block text-center text-custom-red">
+                  {" "}
+                  jan@jankroesche.de
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
