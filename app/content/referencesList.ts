0eebd3daa2764217e4b98e9dde521ad7b59d6be4
Marian Noekel <mariannoekel@gmail.com>
2026-05-05 22:41:42 +0200

    add another reference

--- app/content/referencesList.ts
+++ app/content/referencesList.ts
@@ -1,4 +1,15 @@
 const references = [
+    {
+    title: `Platz 1 Ranking bei Google für den Suchbegriff „Gleitlager Hersteller“`,
+    text: `Unser Kunde ttv wird von uns seit einigen Jahren bei der Website-Optimierung (SEO) begleitet. Durch die umgesetzten Maßnahmen konnten wir die Sichtbarkeit der Website deutlich steigern. Unser Kunde erscheint bei Google für Keywords wie „Gleitlager Hersteller“, „Gleitringdichtungen“, „Massivgleitlager Hersteller“.`,
+    googleResults: `Platz 1 bei Google für „Gleitlager Hersteller“`,
+    preAIresults: "KI: ",
+    aiResults: `Platz 2 bei Google für „Gleitringdichtungen“`,
+    company: `technische teile vertrieb GmbH – Dichtungsspezialist aus Illertissen`,
+    subtext:
+      "Die Firma erhält nun mehr Anfragen von potentiellen Kunden aus dem B2B Bereich, die nach Dichtungen und Gleitlagern im Internet suchen.",
+    image: "/images/screenshots/reference_14.png",
+  },
   {
     title: `Platz 1 Ranking bei Google für den Suchbegriff „Familienhotel Schwabing“`,
     text: `Seit mehreren Jahren optimieren wir die Website des Stadthotels „Schwabinger Wahrheit“ aus München mit dem Ziel, das Hotel bei Google ganz oben zu positionieren, um mehr Hotelgäste zu gewinnen. Das Hotel rankt durch die Optimierungen für viele wichtige Suchbegriffe auf Platz 1 bei Google.`,
