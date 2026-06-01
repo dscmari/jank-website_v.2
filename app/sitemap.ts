f808967c84e63c7b90fb13cf9abac068e8764aef
dscmari <mariannoekel@gmail.com>
2026-06-01 16:32:20 +0200

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -135,6 +135,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: "https://www.jankroesche.de/ueber-jan-kroesche/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: "https://www.jankroesche.de/impressum/",
       lastModified: new Date(),
       changeFrequency: "yearly",
